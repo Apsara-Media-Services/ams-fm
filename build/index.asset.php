7f8a5ef2449c52d6fa2c3d6901b2722203067d4b
Sokmean <47204263+sokmean-mptc@users.noreply.github.com>
2023-06-04 16:28:26 +0700

    #

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '73148e7f0fb96f96e90e');
