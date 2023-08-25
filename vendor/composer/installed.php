e27b4148cf7b13efc221b7eada2c3ec36d091f06
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2023-08-25 17:16:49 +0700

    tag 1.0.2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sokmean-mptc/ams-fm',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '84343c750ecd4a8234046ac0ca7a667eb56d710c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'sokmean-mptc/ams-fm' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '84343c750ecd4a8234046ac0ca7a667eb56d710c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
